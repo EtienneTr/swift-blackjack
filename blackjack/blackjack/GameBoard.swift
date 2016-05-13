2b565bd38ee71dfc4a877cc56541e799d4d23f5a
EtienneTr <supinfo@supinfos-mac.local>
2016-05-13 10:26:52 +0200

    Class game

---
+++ blackjack/blackjack/GameBoard.swift
@@ -0,0 +1,30 @@
+//
+//  GameBoard.swift
+//  blackjack
+//
+//  Created by Supinfo on 13/05/16.
+//  Copyright © 2016 EtienneTR. All rights reserved.
+//
+
+import Foundation
+
+
+class Game {
+    var currShoe : Shoe
+    
+    init(){
+        //init Shoe
+        self.currShoe = Shoe();
+        startGame();
+    }
+    
+    
+    func startGame() {
+        //blue card
+
+        print("Choisissez une carte entre 1 et 312, pour placer la carte bleu");
+        //myInput = numéro de carte, on met la bleu après
+        
+        
+    }
+}
