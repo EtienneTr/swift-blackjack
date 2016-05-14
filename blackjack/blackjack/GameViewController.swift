d4b779f6cc32dc4d97e084e484337c976e77ead0
EtienneTr <supinfo@Supinfos-Mac.local>
2016-05-14 16:10:08 +0200

    outlets

--- blackjack/blackjack/GameViewController.swift
+++ blackjack/blackjack/GameViewController.swift
@@ -27,11 +27,44 @@ class GameViewController: UIViewController {
         //init hand and draw
         game.initHands();
         
+        initFirstDrawView(game.Dealerhand, players: game.PlayersHands)
     }
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    //Game Cards
+    @IBOutlet var DealerCard1: UILabel!
+    @IBOutlet var DealerCard2: UILabel!
 
+    @IBOutlet var UserCard1: UILabel!
+    @IBOutlet var UserCard2: UILabel!
+    
+    @IBOutlet var Player1Card1: UILabel!
+    @IBOutlet var Player1Card2: UILabel!
+    @IBOutlet var Player2Card1: UILabel!
+    @IBOutlet var Player2Card2: UILabel!
+    @IBOutlet var Player4Card1: UILabel!
+    @IBOutlet var Player4Card2: UILabel!
+    
+    
+    func initFirstDrawView(dealer : DealerHand, players : [PlayerHand]){
+        DealerCard1.text = String(dealer.HandCard[0].type!) + " " + String(dealer.HandCard[0].suit!)
+        
+        DealerCard2.text = String(dealer.HandCard[1].type!) + " " + String(dealer.HandCard[1].suit!)
+        
+        UserCard1.text = String(players[0].HandCard[0].type!) + " " + String(players[0].HandCard[0].suit!)
+        UserCard2.text = String(players[0].HandCard[1].type!) + " " + String(players[0].HandCard[1].suit!)
+        
+        Player1Card1.text = String(players[1].HandCard[0].type!) + " " + String(players[1].HandCard[0].suit!)
+        Player1Card2.text = String(players[1].HandCard[1].type!) + " " + String(players[1].HandCard[1].suit!)
+        
+        Player2Card1.text = String(players[2].HandCard[0].type!) + " " + String(players[2].HandCard[0].suit!)
+        Player2Card2.text = String(players[2].HandCard[1].type!) + " " + String(players[2].HandCard[1].suit!)
+        
+        Player4Card1.text = String(players[3].HandCard[0].type!) + " " + String(players[3].HandCard[0].suit!)
+        Player4Card2.text = String(players[3].HandCard[1].type!) + " " + String(players[3].HandCard[1].suit!)
+    }
 }
