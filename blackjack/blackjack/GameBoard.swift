09710b0f9968655aa735196e111108db59409e4b
Nicolas Shih Mendez <nshihmen@gmail.com>
2016-05-16 22:00:48 +0200

    c o m m (u) (n) i (s) t (e)

--- blackjack/blackjack/GameBoard.swift
+++ blackjack/blackjack/GameBoard.swift
@@ -164,7 +164,6 @@ class Game {
                 SplitStatus = 0
                 endRound()
                 return false
-                
             }
             endRound()
             return (false)
